4719e8dc47425e5a75afc3f47c1d22b0353a96f8
Mikhail Andreev <andreemic@gmail.com>
2023-04-29 13:45:09 +0000

    a

--- banana1/app.py
+++ banana1/app.py
@@ -34,11 +34,9 @@ app = Potassium("my_app")
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = whisper.load_model("large-v2", download_root="whisper-cache", device="cuda")
-   
+    
     context = {
-        "model": model
+        "model": None
     }
 
     return context
@@ -46,41 +44,9 @@ def init():
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
-    audio_url = request.json["audio_url"]
-    model = context.get("model")
-
-    audio_path = download_audio_from_url(audio_url)
-    temperature = 0
-    temperature_increment_on_fallback = 0.2
-    if temperature_increment_on_fallback is not None:
-        temperature = tuple(
-            np.arange(temperature, 1.0 + 1e-6, temperature_increment_on_fallback)
-        )
-    else:
-        temperature = [temperature]
     
-    args = {
-        "language": None,
-        "patience": None,
-        "suppress_tokens": "-1",
-        "initial_prompt": None,
-        "condition_on_previous_text": True,
-        "compression_ratio_threshold": 2,
-        "logprob_threshold": -1.0,
-        "no_speech_threshold": 0.6,
-        "word_timestamps": True,
-        "prepend_punctuations": "\"'“¿([{-",
-        "append_punctuations": "\"'.。,，!！?？:：”)]}、"
-    }
-
-    start = time.time()
-    outputs = model.transcribe(str(audio_path), temperature=temperature, **args)
-    end = time.time()
-
-
-    print(f'got outputs in {end - start} seconds: {outputs}')
     return Response(
-        json = {"outputs": outputs}, 
+        json = {"outputs": "hello world"}, 
         status=200
     )
 
